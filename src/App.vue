7988342b35e6c7fdb86c2f57e9fd9fe01a824119
teran <5717131+Dude9303@users.noreply.github.com>
2019-04-15 23:47:43 -0600

    Finished basic header

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item" id="nav-home">
-                    <a class="nav-link" id="logo" href="index.html">Tweeter</a>
-                </li>
-                <li class="nav-item" id="nav-feed">
-                    <a class="nav-link" href="feed.html"><i class="fas fa-home"></i> Feed</a>
-                </li>
-                <li class="nav-item" id="nav-login">
-                    <a class="nav-link" href="login.html"><i class="fas fa-bell"></i> Login</a>
-                </li>
-                <li class="nav-item" id="nav-admin">
-                    <a class="nav-link" href="admin.html"><i class="fas fa-tools"></i> Admin</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" id="search" type="submit"><i
-                        class="fas fa-search"></i></button>
-            </form>
-            <button id="tweet" data-toggle="modal" data-target="#tweetModal">Tweet</button>
-        </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item" id="nav-home">
+            <router-link id="logo" to="/">Tweeter</router-link>
+          </li>
+          <li class="nav-item" id="nav-feed">
+            <router-link class="nav-link" to="/feed"><font-awesome-icon icon="home"/> Feed</router-link>
+          </li>
+          <li class="nav-item" id="nav-login">
+            <router-link class="nav-link" to="/login"><font-awesome-icon icon="bell"/> Login</router-link>
+          </li>
+          <li class="nav-item" id="nav-admin">
+            <router-link class="nav-link" to="/admin"><font-awesome-icon icon="tools"/> Admin</router-link>
+          </li>
+        </ul>
+      </div>
     </nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -45,16 +33,10 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#logo {
+    font-size: 24px;
+    padding-top: 0px;
+    color: rgb(42, 170, 212);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from './router'
 import store from './store'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
+import { faTools } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHome, faBell, faTools)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
