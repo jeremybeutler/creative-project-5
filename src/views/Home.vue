2cf461424116ff7abc3227c122fc1662a70d3659
teran <5717131+Dude9303@users.noreply.github.com>
2019-04-15 23:57:00 -0600

    Added home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-<div class="home"></div>
+<div class="home">
+  <div id="blue">
+    <h2>Stay up to date on all <br>the latest news!</h2>
+  </div>
+  <div id="white">
+    <h2>See what your friends are <br>up to on Tweeter!</h2>
+  </div>
+</div>
 </template>
 
 <script>
@@ -11,3 +18,19 @@ export default {
   },
 }
 </script>
+
+<style>
+#blue {
+  width: 50%;
+  height: 100%;
+  position: absolute;
+  background-color: #1da1f2;
+  color: white;
+  padding-top: 20%;
+}
+#white {
+  float: right;
+  margin-top: 20%;
+  width: 50%;
+}
+</style>
