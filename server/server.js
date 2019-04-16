b6a068f179d1e99fb3b3693a40fcbdefd94c2c08
teran <5717131+Dude9303@users.noreply.github.com>
2019-04-15 22:58:07 -0600

    Added authentication system

--- server/server.js
+++ server/server.js
@@ -26,4 +26,4 @@ app.use("/api/tweets", tweets.routes);
 const comments = require("./comments.js");
 app.use("/api/comments", comments.routes);
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3001!'));
---
+++ src/views/Feed.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+  <div v-if="user">
+    <div class="header">
+      <div>
+        <h1>{{user.name}}</h1>
+      </div>
+      <div>
+        <p>
+          <a href="#" @click="logout"><i class="fas fa-sign-out-alt"></i></a>
+        </p>
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    <p>If you would like to access your feed,, please register for an account or login.</p>
+    <router-link to="/register" class="pure-button">Register</router-link> or
+    <router-link to="/login" class="pure-button">Login</router-link>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'feed',
+  components: {
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+  },
+  async created() {
+    await this.$store.dispatch("getUser");
+  },
+  methods: {
+    async logout() {
+      try {
+        this.error = await this.$store.dispatch("logout");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+}
+
+.header a {
+  padding-left: 50px;
+  color: #222;
+  font-size: 2em;
+}
+
+.header svg {
+  margin-top: 12px;
+}
+</style>
